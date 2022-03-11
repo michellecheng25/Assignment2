a73478ae3682c56fea70e3f1e0e5a4027f333255
Michelle Cheng <michellecheng.2017@gmail.com>
2022-03-11 15:41:36 -0500

    implemented and tested mySome

--- app.js
+++ app.js
@@ -1,49 +1,61 @@
 // FOR EACH //
-Array.prototype.myEach = function() {
+Array.prototype.myEach = function () {
   // Place your code here.
 };
 
 // MAP //
-Array.prototype.myMap = function() {
+Array.prototype.myMap = function () {
   // Place your code here.
 };
 
 // SOME //
-Array.prototype.mySome = function() {
-  // Place your code here.
+Array.prototype.mySome = function (callbackFn) {
+  for (let i = 0; i < this.length; i++) {
+    if (this[i] === undefined) continue;
+    let result = callbackFn(this[i], i, this);
+    if (result == true) return true;
+  }
+  return false;
 };
 
+const array = [1, 2, 3, 4, 5];
+// checks whether an element is even
+const even = (element) => element % 2 === 0;
+console.log(array.some(even));
+// expected output: true
+console.log(array.mySome(even));
+
 // REDUCE //
-Array.prototype.myReduce = function() {
+Array.prototype.myReduce = function () {
   // Place your code here.
 };
 
 // INCLUDES //
-Array.prototype.myIncludes = function() {
+Array.prototype.myIncludes = function () {
   // Place your code here.
 };
 
 // INDEXOF //
-Array.prototype.myIndexOf = function() {
+Array.prototype.myIndexOf = function () {
   // Place your code here.
 };
 
 // PUSH //
-Array.prototype.myPush = function() {
+Array.prototype.myPush = function () {
   // Place your code here.
 };
 
 // LASTINDEXOF //
-Array.prototype.myLastIndexOf = function() {
+Array.prototype.myLastIndexOf = function () {
   // Place your code here.
 };
 
 // KEYS //
-Object.myKeys = function() {
+Object.myKeys = function () {
   // Place your code here.
 };
 
 // VALUES //
-Object.myValues = function() {
+Object.myValues = function () {
   // Place your code here.
 };
