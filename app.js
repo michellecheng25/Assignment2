3fce56a2e7b6d136205bd97f18d271150d1d9427
Michelle Cheng <michellecheng.2017@gmail.com>
2022-03-11 15:49:46 -0500

    implemented and tested myIndexOf

--- app.js
+++ app.js
@@ -1,49 +1,76 @@
 // FOR EACH //
-Array.prototype.myEach = function() {
+Array.prototype.myEach = function () {
   // Place your code here.
 };
 
 // MAP //
-Array.prototype.myMap = function() {
+Array.prototype.myMap = function () {
   // Place your code here.
 };
 
 // SOME //
-Array.prototype.mySome = function() {
+Array.prototype.mySome = function () {
   // Place your code here.
 };
 
 // REDUCE //
-Array.prototype.myReduce = function() {
+Array.prototype.myReduce = function () {
   // Place your code here.
 };
 
 // INCLUDES //
-Array.prototype.myIncludes = function() {
+Array.prototype.myIncludes = function () {
   // Place your code here.
 };
 
 // INDEXOF //
-Array.prototype.myIndexOf = function() {
-  // Place your code here.
+Array.prototype.myIndexOf = function (searchElement, fromIndex) {
+  var fromIndex = fromIndex == undefined ? 0 : fromIndex;
+  for (let i = fromIndex; i < this.length; i++) {
+    if (this[i] === undefined) continue;
+    if (this[i] === searchElement) return i;
+  }
+  return -1;
 };
 
+const beasts = ["ant", "bison", "camel", "duck", "bison"];
+
+console.log(beasts.indexOf("bison"));
+// expected output: 1
+
+// start from index 2
+console.log(beasts.indexOf("bison", 2));
+// expected output: 4
+
+console.log(beasts.indexOf("giraffe"));
+// expected output: -1
+
+console.log(beasts.myIndexOf("bison"));
+// expected output: 1
+
+// start from index 2
+console.log(beasts.myIndexOf("bison", 2));
+// expected output: 4
+
+console.log(beasts.myIndexOf("giraffe"));
+// expected output: -1
+
 // PUSH //
-Array.prototype.myPush = function() {
+Array.prototype.myPush = function () {
   // Place your code here.
 };
 
 // LASTINDEXOF //
-Array.prototype.myLastIndexOf = function() {
+Array.prototype.myLastIndexOf = function () {
   // Place your code here.
 };
 
 // KEYS //
-Object.myKeys = function() {
+Object.myKeys = function () {
   // Place your code here.
 };
 
 // VALUES //
-Object.myValues = function() {
+Object.myValues = function () {
   // Place your code here.
 };
