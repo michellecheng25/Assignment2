d8958c6d4e4615cbbdb3126a8d964542dfde80f7
Michelle Cheng <michellecheng.2017@gmail.com>
2022-03-11 16:35:20 -0500

    reformatting

--- app.js
+++ app.js
@@ -21,17 +21,6 @@ Array.prototype.myMap = function (callbackFn) {
   return result;
 };
 
-console.log("Testing map!");
-const array1 = [1, 4, , 9, 16];
-// pass a function to map
-const map1 = array1.map((x) => x * 2);
-console.log(map1);
-// expected output: Array [2, 8, 18, 32]
-console.log("Testing myMap!");
-const map2 = array1.myMap((x) => x * 2);
-console.log(map2);
-console.log("--------");
-
 // SOME //
 Array.prototype.mySome = function (callbackFn) {
   for (let i = 0; i < this.length; i++) {
@@ -41,13 +30,6 @@ Array.prototype.mySome = function (callbackFn) {
   }
   return false;
 };
-
-const array = [1, 2, 3, 4, 5];
-// checks whether an element is even
-const even = (element) => element % 2 === 0;
-console.log(array.some(even));
-// expected output: true
-console.log(array.mySome(even));
 
 // REDUCE //
 Array.prototype.myReduce = function (callbackFn, initialValue) {
@@ -62,25 +44,6 @@ Array.prototype.myReduce = function (callbackFn, initialValue) {
   return result;
 };
 
-const arrayReduce = [1, 2, 3, 4];
-
-// 0 + 1 + 2 + 3 + 4
-const initialValue = 0;
-const sumWithInitial = arrayReduce.reduce(
-  (previousValue, currentValue) => previousValue + currentValue,
-  initialValue
-);
-
-console.log(sumWithInitial);
-// expected output: 10
-
-const sumWithInitial2 = arrayReduce.myReduce(
-  (previousValue, currentValue) => previousValue + currentValue,
-  initialValue
-);
-
-console.log(sumWithInitial2);
-
 // INCLUDES //
 Array.prototype.myIncludes = function (searchElement, fromIndex) {
   var fromIndex = fromIndex == undefined ? 0 : fromIndex;
@@ -91,28 +54,6 @@ Array.prototype.myIncludes = function (searchElement, fromIndex) {
   return false;
 };
 
-const array2 = [1, 2, 3];
-
-console.log(array2.includes(2));
-// expected output: true
-
-const pets = ["cat", "dog", "bat"];
-
-console.log(pets.includes("cat"));
-// expected output: true
-
-console.log(pets.includes("at"));
-// expected output: false
-
-console.log(array2.myIncludes(2));
-// expected output: true
-
-console.log(pets.myIncludes("cat"));
-// expected output: true
-
-console.log(pets.myIncludes("at"));
-// expected output: false
-
 // INDEXOF //
 Array.prototype.myIndexOf = function (searchElement, fromIndex) {
   var fromIndex = fromIndex == undefined ? 0 : fromIndex;
@@ -122,28 +63,6 @@ Array.prototype.myIndexOf = function (searchElement, fromIndex) {
   }
   return -1;
 };
-
-const beasts = ["ant", "bison", "camel", "duck", "bison"];
-
-console.log(beasts.indexOf("bison"));
-// expected output: 1
-
-// start from index 2
-console.log(beasts.indexOf("bison", 2));
-// expected output: 4
-
-console.log(beasts.indexOf("giraffe"));
-// expected output: -1
-
-console.log(beasts.myIndexOf("bison"));
-// expected output: 1
-
-// start from index 2
-console.log(beasts.myIndexOf("bison", 2));
-// expected output: 4
-
-console.log(beasts.myIndexOf("giraffe"));
-// expected output: -1
 
 Array.prototype.myPush = function (...args) {
   let args_index = 0; // Index of new element
@@ -166,20 +85,6 @@ Array.prototype.myLastIndexOf = function (searchElement, fromIndex) {
   return -1;
 };
 
-const animals = ["Dodo", "Tiger", "Penguin", "Dodo"];
-
-console.log(animals.lastIndexOf("Dodo"));
-// expected output: 3
-
-console.log(animals.lastIndexOf("Tiger"));
-// expected output: 1
-
-console.log(animals.myLastIndexOf("Dodo"));
-// expected output: 3
-
-console.log(animals.myLastIndexOf("Tiger"));
-// expected output: 1
-
 // KEYS //
 Object.myKeys = function (obj) {
   const result = [];
@@ -189,18 +94,6 @@ Object.myKeys = function (obj) {
   return result;
 };
 
-const object1 = {
-  a: "somestring",
-  b: 42,
-  c: false,
-};
-
-console.log(Object.keys(object1));
-// expected output: Array ["a", "b", "c"]
-
-console.log(Object.myKeys(object1));
-// expected output: Array ["a", "b", "c"]
-
 // VALUES //
 Object.myValues = function (obj) {
   const result = [];
@@ -209,15 +102,3 @@ Object.myValues = function (obj) {
   }
   return result;
 };
-
-const object2 = {
-  a: "somestring",
-  b: 42,
-  c: false,
-};
-
-console.log(Object.values(object2));
-// expected output: Array ["somestring", 42, false]
-
-console.log(Object.myValues(object2));
-// expected output: Array ["somestring", 42, false]
