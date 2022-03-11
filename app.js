23e861f733c25c4a014656a54a61070d3408e0e0
Michelle Cheng <michellecheng.2017@gmail.com>
2022-03-11 15:51:33 -0500

    implemented myPush using professor's example code

--- app.js
+++ app.js
@@ -1,49 +1,56 @@
 // FOR EACH //
-Array.prototype.myEach = function() {
+Array.prototype.myEach = function () {
   // Place your code here.
 };
 
 // MAP //
-Array.prototype.myMap = function() {
+Array.prototype.myMap = function () {
   // Place your code here.
 };
 
 // SOME //
-Array.prototype.mySome = function() {
+Array.prototype.mySome = function () {
   // Place your code here.
 };
 
 // REDUCE //
-Array.prototype.myReduce = function() {
+Array.prototype.myReduce = function () {
   // Place your code here.
 };
 
 // INCLUDES //
-Array.prototype.myIncludes = function() {
+Array.prototype.myIncludes = function () {
   // Place your code here.
 };
 
 // INDEXOF //
-Array.prototype.myIndexOf = function() {
+Array.prototype.myIndexOf = function () {
   // Place your code here.
 };
 
 // PUSH //
-Array.prototype.myPush = function() {
-  // Place your code here.
+Array.prototype.myPush = function (...args) {
+  let args_index = 0; // Index of new element
+  let length = this.length; // Length of "this" array
+  // The last element of "this" array is at length - 1
+  for (let i = length; i < length + args.length; i++) {
+    this[i] = args[args_index]; // Add new element to end of "this" array
+    args_index++;
+  }
+  return this.length; // Return new length of "this" array
 };
 
 // LASTINDEXOF //
-Array.prototype.myLastIndexOf = function() {
+Array.prototype.myLastIndexOf = function () {
   // Place your code here.
 };
 
 // KEYS //
-Object.myKeys = function() {
+Object.myKeys = function () {
   // Place your code here.
 };
 
 // VALUES //
-Object.myValues = function() {
+Object.myValues = function () {
   // Place your code here.
 };
